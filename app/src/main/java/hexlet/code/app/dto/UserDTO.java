fc049c18861b51953f47c678f4f701e13462bc09
Denis Orekhov <3008denis@gmail.com>
2024-07-15 15:19:28 +0500

    Rm unused imports

--- app/src/main/java/hexlet/code/app/dto/UserDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserDTO.java
@@ -1,14 +1,5 @@
 package hexlet.code.app.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import java.time.LocalDate;
 
 public class UserDTO {
