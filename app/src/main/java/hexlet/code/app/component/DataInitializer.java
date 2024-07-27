081db00abd371fa4d60b509d82916121f4043caa
Denis Orekhov <3008denis@gmail.com>
2024-07-27 12:04:29 +0500

    Fix bug with DataInitializer

--- app/src/main/java/hexlet/code/app/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -2,7 +2,12 @@ package hexlet.code.app.component;
 
 import hexlet.code.app.dto.LabelCreatedDTO;
 import hexlet.code.app.dto.TaskStatusCreatedDTO;
+import hexlet.code.app.model.Label;
+import hexlet.code.app.model.TaskStatus;
 import hexlet.code.app.model.User;
+import hexlet.code.app.repository.LabelRepository;
+import hexlet.code.app.repository.TaskStatusRepository;
+import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.service.CustomUserDetailsService;
 import hexlet.code.app.service.LabelService;
 import hexlet.code.app.service.TaskStatusService;
@@ -27,14 +32,25 @@ public class DataInitializer implements ApplicationRunner {
     @Autowired
     private final LabelService labelService;
 
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private final LabelRepository labelRepository;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var email = "hexlet@example.com";
-        var userData = new User();
-        userData.setEmail(email);
-        userData.setPassword("qwerty");
-        userService.createUser(userData);
+        if (userRepository.findByEmail("hexlet@example.com").isEmpty()) {
+            var email = "hexlet@example.com";
+            var userData = new User();
+            userData.setEmail(email);
+            userData.setPassword("qwerty");
+            userService.createUser(userData);
+        }
 
         var defaultStatuses = new ArrayList<TaskStatusCreatedDTO>();
         defaultStatuses.add(new TaskStatusCreatedDTO("Draft", "draft"));
@@ -42,13 +58,21 @@ public class DataInitializer implements ApplicationRunner {
         defaultStatuses.add(new TaskStatusCreatedDTO("toBeFixed", "to_be_fixed"));
         defaultStatuses.add(new TaskStatusCreatedDTO("toPublish", "to_publish"));
         defaultStatuses.add(new TaskStatusCreatedDTO("Published", "published"));
+        var currentStatuses = taskStatusRepository.findAll().stream().map(TaskStatus::getSlug).toList();
         for (var status : defaultStatuses) {
-            taskStatusService.create(status);
+            if (!currentStatuses.contains(status.getSlug())) {
+                taskStatusService.create(status);
+            }
         }
 
         var defaultLabels = new ArrayList<LabelCreatedDTO>();
         defaultLabels.add(new LabelCreatedDTO("feature"));
         defaultLabels.add(new LabelCreatedDTO("bug"));
-        defaultLabels.stream().peek(labelService::create).toList();
+        var currentLabels = labelRepository.findAll().stream().map(Label::getName).toList();
+        for (var label : defaultLabels) {
+            if (!currentLabels.contains(label.getName())) {
+                labelService.create(label);
+            }
+        }
     }
 }
