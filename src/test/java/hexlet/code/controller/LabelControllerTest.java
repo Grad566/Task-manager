42dc9b5541b62bdd35ae4ee6026ef65295e947a2
Denis Orekhov <3008denis@gmail.com>
2024-08-01 13:19:21 +0500

    Refactoring LabelTest

--- src/test/java/hexlet/code/controller/LabelControllerTest.java
+++ src/test/java/hexlet/code/controller/LabelControllerTest.java
@@ -2,15 +2,16 @@ package hexlet.code.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.dto.LabelCreatedDTO;
+import hexlet.code.dto.LabelUpdatedDTO;
 import hexlet.code.model.Label;
-import hexlet.code.model.Task;
 import hexlet.code.repository.LabelRepository;
-import hexlet.code.repository.TaskRepository;
 import hexlet.code.util.ModelGenerator;
 import org.assertj.core.api.Assertions;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,15 +40,10 @@ class LabelControllerTest {
     private LabelRepository labelRepository;
 
     @Autowired
-    private TaskRepository taskRepository;
-
-    @Autowired
     private ModelGenerator modelGenerator;
 
     @Autowired
     private ObjectMapper om;
-
-    private Task testTask;
 
     private Label testLabel;
 
@@ -56,11 +52,8 @@ class LabelControllerTest {
     @BeforeEach
     public void setUp() {
         token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
-        testTask = Instancio.of(modelGenerator.getTaskModel()).create();
         testLabel = Instancio.of(modelGenerator.getLabelModel()).create();
 
-//        taskRepository.save(testTask);
-//        testLabel.setTasks(new ArrayList<Task>(List.of(testTask)));
         labelRepository.save(testLabel);
 
     }
@@ -79,9 +72,7 @@ class LabelControllerTest {
 
     @Test
     public void testCreate() throws Exception {
-        var data = new HashMap<String, String>(Map.of(
-                "name", "testName"
-        ));
+        var data = new LabelCreatedDTO("testName");
 
         var request = post("/api/labels")
                 .with(token)
@@ -90,17 +81,15 @@ class LabelControllerTest {
 
         mockMvc.perform(request).andExpect(status().isCreated());
 
-        var label = labelRepository.findByName(data.get("name")).get();
+        var label = labelRepository.findByName(data.getName()).get();
 
         assertNotNull(label);
-        Assertions.assertThat(label.getName()).isEqualTo(data.get("name"));
+        Assertions.assertThat(label.getName()).isEqualTo(data.getName());
     }
 
     @Test
     public void testUpdate() throws Exception {
-        var data = new HashMap<String, String>(Map.of(
-                "name", "newName"
-        ));
+        var data = new LabelUpdatedDTO(JsonNullable.of("newName"));
 
         var request = put("/api/labels/" + testLabel.getId())
                 .with(token)
@@ -109,7 +98,7 @@ class LabelControllerTest {
 
         mockMvc.perform(request).andExpect(status().isOk());
 
-        var updatedLabel = labelRepository.findByName(data.get("name")).get();
+        var updatedLabel = labelRepository.findByName(data.getName().get()).get();
 
         assertNotNull(updatedLabel);
     }
