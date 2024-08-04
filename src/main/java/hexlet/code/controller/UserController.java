289df659188252fb554f3fa41148cd6e391ad649
Denis Orekhov <3008denis@gmail.com>
2024-08-04 15:11:40 +0500

    Fix code style

--- src/main/java/hexlet/code/controller/UserController.java
+++ src/main/java/hexlet/code/controller/UserController.java
@@ -7,7 +7,6 @@ import hexlet.code.dto.userDTO.UserUpdatedDTO;
 import hexlet.code.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
